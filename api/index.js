6ecdf4c5e63fe0e4a761c416f7f08026deaa6421
Florent Poujol <florent.poujol@gmail.com>
2016-01-08 08:45:59 +0100

    Update to new API plugin registration.

--- api/index.js
+++ api/index.js
@@ -1,6 +1,6 @@
 var fs = require("fs");
 
-SupCore.system.api.registerPlugin("typescript", "fText", {
+SupCore.system.registerPlugin("typescriptAPI", "fText", {
   code: fs.readFileSync(__dirname+"/fText.ts", { encoding: "utf8" }).replace("<reference path=", ""),
   defs: fs.readFileSync(__dirname+"/fText.d.ts", { encoding: "utf8" }),
 });
